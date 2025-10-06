4482533ab7a4763f798fa982a9b63dd26b9eb87c
Akhil Tilak <akhil.tilak@webofficeit.com>
2025-10-06 12:45:19 +0530

    add.tsx file changed

--- components/job/add.tsx
+++ components/job/add.tsx
@@ -15,7 +15,7 @@ import dayjs from "dayjs";
 
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 const initalForm: CreateJobData = {
-    price_value: null,
+    price_value: null, 
     title: "",
     subtitle: "",
     description: "",
