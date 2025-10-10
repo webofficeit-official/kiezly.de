8ca487636bb193413cde59e6d5b40455d21c99bb
Rahul R S <rahul.rs@webofficeit.com>
2025-10-10 15:51:08 +0530

    location details

---
+++ app/(protected)/wizard/location-details/page.tsx
@@ -0,0 +1,87 @@
+"use client";
+import { Card } from "@/components/ui/card";
+import { WizardDirection } from "@/components/wizard/WizardDirection";
+import { WizardHeader } from "@/components/wizard/WizardHeader";
+import { WizardInput } from "@/components/wizard/WizardInput";
+import { WizardMultiSelect } from "@/components/wizard/WizardMultiSelect";
+import { WizardNavigation } from "@/components/wizard/WizardNavigation";
+import { WizardRichText } from "@/components/wizard/WizardRichText";
+import { WizardSelect } from "@/components/wizard/WizardSelect";
+import React from "react";
+
+export default function Page() {
+    return (
+        <>
+            <div className="min-h-screen bg-gray-50 text-gray-900">
+                <main className="max-w-6xl mx-auto px-4 py-6 grid grid-cols-1 lg:grid-cols-3 gap-6">
+                    {/* Post Job */}
+                    <section className="lg:col-span-3 space-y-4">
+                        {/* Header */}
+                        <div className="bg-white rounded-2xl shadow-sm border p-4 sm:p-6 flex items-center justify-between">
+                            <div>
+                                <h2 className="text-lg font-semibold">Post a mini-job</h2>
+                            </div>
+                            <div className="flex items-center gap-4 w-45">
+                            </div>
+                        </div>
+                    </section>
+                    <Card className="lg:col-span-3 space-y-4">
+                        <div className="grid grid-cols-12 gap-4">
+                            <div className="col-span-12 sm:col-span-6 lg:col-span-5 bg-gray-100 p-6">
+                                <div className="flex flex-wrap justify-center lg:flex-col gap-2  justify-between lg:space-x-0 lg:space-y-4">
+                                    <WizardNavigation title="Basic Details" description="Provide the main information" count={1} current={false} finished={true} />
+                                    <WizardNavigation title="Job Details" description="Provide detailed information" count={2} current={false} finished={true} />
+                                    <WizardNavigation title="Location Details" description="Provide location details" count={3} current={true} finished={false} />
+                                    <WizardNavigation title="Pricing Details" description="Set the pricing for this job" count={4} current={false} finished={false} />
+                                    <WizardNavigation title="Work Details" description="Provide work details" count={5} current={false} finished={false} />
+                                    <WizardNavigation title="Contact Details" description="Provide how applicants can reach you" count={6} current={false} finished={false} />
+                                </div>
+                            </div>
+
+                            <div className="col-span-12 sm:col-span-6 lg:col-span-7 bg-white p-6">
+                                {/* --- Header --- */}
+                                <WizardHeader title="Location Details" description="Provide location details" />
+
+                                {/* Form */}
+                                <div className="mt-5">
+                                    <div className="flex flex-col md:flex-row">
+                                        <WizardSelect
+                                            label="Country"
+                                            value='1'
+                                            onChange={() => { }}
+                                            options={[
+                                                {
+                                                    label: "Germany",
+                                                    value: '1'
+                                                },
+                                                {
+                                                    label: "India",
+                                                    value: '3'
+                                                },
+                                                {
+                                                    label: "Australia",
+                                                    value: '2'
+                                                }
+                                            ]}
+                                        />
+                                        <WizardInput label="Postal Code" placeholder="10707" value='' onChange={() => { }} />
+                                    </div>
+                                    <div className="flex flex-col md:flex-row">
+                                        <WizardInput label="Street" placeholder="Essen" value='' onChange={() => { }} />
+                                        <WizardInput label="City" placeholder="Reg.-Bez. Düsseldorf" value='' onChange={() => { }} />
+                                        <WizardInput label="State" placeholder="Nordrhein-Westfalen" value='' onChange={() => { }} />
+                                    </div>
+                                    <div className="flex flex-col md:flex-row">
+                                        <WizardInput label="Latitude" placeholder="51.4535" value='' onChange={() => { }} />
+                                        <WizardInput label="Longitude" placeholder="7.0102" value='' onChange={() => { }} />
+                                    </div>
+                                    <WizardDirection next prev prevLink="/wizard/job-details" nextLink="/wizard/pricing-details" />
+                                </div>
+                            </div>
+                        </div>
+                    </Card>
+                </main>
+            </div>
+        </>
+    );
+}
