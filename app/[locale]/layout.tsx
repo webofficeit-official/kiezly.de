d339bf7dbde159b60adc6662146f00d4f4373996
Rahul R S <rahul.rs@webofficeit.com>
2025-10-22 10:28:23 +0530

    404 page

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -72,7 +72,7 @@ export default function LocaleLayout({ children, params }: any) {
   const [messages, setMessages] = useState<any>({});
 
   // List of JSON files to load for each locale
-  const files = ["header", "footer", "home", "signup", "signin", "howItWorks", "impressum", "terms", "privacy"]; // add more as needed
+  const files = ["header", "footer", "home", "signup", "signin", "howItWorks", "impressum", "terms", "privacy", "404"]; // add more as needed
 
   useEffect(() => {
     async function loadMessages() {
